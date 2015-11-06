5f3e9ebfd47620f8010e7f5656900305f9e416bf
Chris <chris@linux-1anr.site>
2015-11-06 12:06:06 +0100

    Submission Homework

--- logmap.cxx
+++ logmap.cxx
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -7,6 +8,9 @@ int main(){
 	double x;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	
+	float x_values[Nend-Nskip];
+	ofstream out("output.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
@@ -14,9 +18,17 @@ int main(){
 		   x = r*x*(1-x);
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   //cout << r << "\t" << x << endl;
+			   //save x_101 in in x_values[0] and so forth
+			   x_values[i-Nskip-1]= x;
    	   }
+   	   //print r and current x_i values to file
+   	   for(int i=1; i<=100; i++){
+			   out << r << "\t" << x_values[i-1] << endl;
+	   }
+   	   
 	}
+	out.close();
 
 
 	return 0;
